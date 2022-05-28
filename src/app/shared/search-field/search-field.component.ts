fd08c6309144be21c6674e5eaf554278af92ca97
Andre Steudel <andre.steudel@yellowmap.de>
2022-05-28 14:25:24 +0200

    add search field component

---
+++ src/app/shared/search-field/search-field.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+  Subscription,
+} from 'rxjs';
+
+@Component({
+  selector: 'app-search-field',
+  templateUrl: './search-field.component.html',
+  styleUrls: ['./search-field.component.scss'],
+})
+export class SearchFieldComponent implements OnInit {
+  @Output() search = new EventEmitter<string>();
+  private searchAsYouType = new Subject<string>();
+  private textValue: string = '';
+  private searchAsYouTypeSubscription?: Subscription;
+
+  ngOnInit() {
+    this.searchAsYouTypeSubscription = this.searchAsYouType
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((newValue) => {
+        this.search.emit(newValue);
+      });
+  }
+
+  onEnter(_event: Event) {
+    this.search.emit(this.textValue);
+  }
+
+  onInput(e: Event) {
+    this.textValue = (e.target as HTMLInputElement).value;
+    this.searchAsYouType.next(this.textValue);
+  }
+
+  ngOnDestroy() {
+    if (this.searchAsYouTypeSubscription) {
+      this.searchAsYouTypeSubscription.unsubscribe();
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from './layout/layout.module';
 import { AvatarComponent } from './avatar/avatar.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   declarations: [
-    AvatarComponent
+    AvatarComponent,
+    SearchFieldComponent
   ],
   exports: [
     LayoutModule,
-    AvatarComponent
+    AvatarComponent,
+    SearchFieldComponent
   ]
 })
 export class SharedModule {}
