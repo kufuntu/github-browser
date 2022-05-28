77115c62210440422c9eeb171bfa754100963810
Andre Steudel <andre.steudel@yellowmap.de>
2022-05-28 11:40:44 +0200

    add avatar component

---
+++ src/app/shared/avatar/avatar.component.ts
@@ -0,0 +1,15 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AvatarComponent {
+  @Input() src?:string;
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [],
-  exports: [LayoutModule]
+  declarations: [
+    AvatarComponent
+  ],
+  exports: [
+    LayoutModule,
+    AvatarComponent
+  ]
 })
 export class SharedModule {}
