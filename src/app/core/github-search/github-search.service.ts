5c10c3278c7ca8517950a76137bf38a377dd5a93
Andre Steudel <andre.steudel@yellowmap.de>
2022-05-28 15:33:27 +0200

    catch http errors

--- src/app/core/github-search/github-search.service.ts
+++ src/app/core/github-search/github-search.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
 import { GithubRepository } from '../types/repository';
 import { GithubResponse } from '../types/response';
 import { environment } from '../../../environments/environment';
 import { GithubCommit } from '../types/commit';
+
+const EMPTY_RESPONSE: GithubResponse = {
+  total_count: 0,
+  incomplete_results: false,
+  items: []
+};
 
 @Injectable()
 export class GithubSearch {
@@ -15,6 +21,8 @@ export class GithubSearch {
     return this.http.get<GithubResponse<GithubRepository>>(
       `${environment.githubUrl}/search/repositories`,
       { params: { q: query } }
+    ).pipe(
+      catchError(() => of(EMPTY_RESPONSE))
     );
   }
 
@@ -23,6 +31,8 @@ export class GithubSearch {
     return this.http.get<GithubResponse<GithubCommit>>(
       `${environment.githubUrl}/search/commits`,
       { params: { q: query } }
+    ).pipe(
+      catchError(() => of(EMPTY_RESPONSE))
     );
   }
 }
